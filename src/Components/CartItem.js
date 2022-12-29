75a3e8354b680c3bbc1d880c32de213d75ae1ab9
ashiqurras <ashiqurrussel2@gmail.com>
2022-12-29 21:21:35 +0100

    display product on sidebar

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdClose } from "react-icons/io";
+const CartItem = ({ product }) => {
+  //desctrutring the product
+  const { id, title, image, price, amount } = product;
+  return (
+    <div className="flex">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        <Link to={`/product/${id}`}>
+          {" "}
+          <img className="max-w-[80px]" src={image} alt="" />
+        </Link>{" "}
+        <div className="w-full flex-col">
+          <div className=" flex items-center justify-between">
+            <Link
+              to={`/product/${id}`}
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+            >
+              {title}
+            </Link>
 
-const CartItem = () => {
-  return <div>cart Item</div>;
+            <IoMdClose className="text-gray-500 hover:text-red-500 transition"></IoMdClose>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,10 +3,12 @@ import { SidebarContext } from "../contexts/SidebarContextProvider";
 import { IoMdArrowForward, IoMdClose } from "react-icons/io";
 import { FiTrash2 } from "react-icons/fi";
 import { CartContext } from "../contexts/CartContextProvider";
+import CartItem from "./CartItem";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
   const { cart } = useContext(CartContext);
+
   console.log("sidebar cart ", cart);
 
   return (
@@ -16,7 +18,10 @@ const Sidebar = () => {
       } w-full bg-white top-0 h-full fixed shadow-2xl md:w-[35vw] xl:max-w-[30vw] duration-300 z-20 px-4 md:px-[35px] lg:px-[35px]`}
     >
       <div className="flex items-center justify-between py-6 border-b ">
-        <div className="uppercase text-xm font-semibold"> Shopping Bag 0</div>
+        <div className="uppercase text-xm font-semibold">
+          {" "}
+          Shopping Bag {cart.length}
+        </div>
         <div
           onClick={() => handleClose(!isOpen)}
           className="cursor-pointer w-8 h-8 flex justify-center items-center text-2xl"
@@ -26,7 +31,7 @@ const Sidebar = () => {
       </div>
       <div>Cart Item</div>
       {cart.map((product) => (
-        <div>{product.title}</div>
+        <CartItem key={product.id} product={product}></CartItem>
       ))}
     </div>
   );
